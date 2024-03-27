8011df483c1e07bd7aa58bb86ac461a1dbe0961b
vanhuu2409 <vanhuu0924@gmail.com>
2024-03-27 23:03:48 +0700

    create middleware and handler func possibles error

--- backend/index.js
+++ backend/index.js
@@ -36,6 +36,11 @@ connect(process.env.MONGODB)
 // API Route Creation
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({ success: false, statusCode, message });
+});
 
 // Image Storage Engine
 const storage = diskStorage({
