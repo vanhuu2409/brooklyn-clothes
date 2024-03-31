ac4dabb01114f3a45f7ce8c78e9a306ed920dec0
vanhuu2409 <vanhuu0924@gmail.com>
2024-03-31 22:33:21 +0700

    config vite fe

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
+    build: {
+      target: "esnext", //browsers can handle the latest ES features
+    },
     server: {
       port: 8080,
       proxy: {
